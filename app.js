6b250fbd24def18d438180718939608bd0847583
Anthony <yefremov.anthony@gmail.com>
2022-06-03 14:06:10 +0300

    refactor: add alt for new buttons (Fri, Jun 3, 2022 2:05:55 PM)

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button button_delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = 'button_delete__image';
+    deleteButtonImg.setAttribute("alt","remove button");
     deleteButton.appendChild(deleteButtonImg);
 
 
